0b5b2cb15091018f42ee27972180355d8b10dd81
Marsalov Sergey <marsalovv@gmail.com>
2023-01-25 17:16:09 +0300

    2.3

---
+++ navigation/Profile/User/RealmManager.swift
@@ -0,0 +1,36 @@
+import Foundation
+import RealmSwift
+
+class RealmManager {
+    
+    static let manager = RealmManager()
+    
+    init() {
+        let config = Realm.Configuration(schemaVersion: 2)
+        Realm.Configuration.defaultConfiguration = config
+    }
+    func userSave() -> RealmUserModel? {
+        let realm = try! Realm()
+        for user in realm.objects(RealmUserModel.self) {
+            return user
+        }
+        return nil
+    }
+    
+    func login(email: String, password: String) -> RealmUserModel{
+        let realm = try! Realm()
+        let user = RealmUserModel(email: email, password: password)
+        
+        try! realm.write {
+            realm.add(user)
+        }
+        return user
+    }
+    
+    func deleteAll() {
+        let realm = try! Realm()
+        try! realm.write {
+            realm.deleteAll()
+        }
+    }
+}
